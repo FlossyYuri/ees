eadd04868975c93f92139d0440b980d1f9efb39e
Flossy Yuri <emerson.yur@gmail.com>
2022-11-16 18:32:42 +0200

    Add header

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main': '#EC7644',
+        'darker': '#212121',
+      }
+    },
+  },
+  plugins: [],
+}
