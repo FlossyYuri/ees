d896f30cff627aa8c355a79b661fd7b8381f6a75
Flossy Yuri <emerson.yur@gmail.com>
2022-12-14 18:54:40 +0200

    fix articles type

--- pages/api/articles.ts
+++ pages/api/articles.ts
@@ -1,11 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../lib/dbConnect';
-import Article from '../../lib/models/Article';
-
-type Data = {
-  name: string;
-};
+import * as Article from '../../lib/models/Article';
 
 export default async function handler(
   req: NextApiRequest,
@@ -18,7 +14,7 @@ export default async function handler(
   switch (method) {
     case 'GET':
       try {
-        const articles = await Article.find({});
+        const articles = await (Article as any).find({});
         res.status(200).json({ success: true, data: articles });
       } catch (error) {
         res.status(400).json({ success: false });
@@ -26,7 +22,7 @@ export default async function handler(
       break;
     case 'POST':
       try {
-        const article = await Article.create(req.body);
+        const article = await (Article as any).create(req.body);
         res.status(201).json({ success: true, data: article });
       } catch (error) {
         res.status(400).json({ success: false });
