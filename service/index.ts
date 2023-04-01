399ef78b292b8e8901e2bda1e5857cec5569dc37
Flossy Yuri <emerson.yur@gmail.com>
2023-04-01 12:58:52 +0200

    fix: set api url to env

--- service/index.ts
+++ service/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const REQUEST_TIMEOUT = 90 * 1000;
 export const APIKit = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.API_URL,
   timeout: REQUEST_TIMEOUT,
 });
 
